74560bf38dc6519626bf3bb4921cef93f09686f3
BuithanhtungHUB <buithanhtung100496@gmail.com>
2021-08-31 22:04:09 +0700

    set model

---
+++ app/Models/Review.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    public function house()
+    {
+        return $this->belongsTo(House::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function houses()
+    {
+        return $this->hasMany(House::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
