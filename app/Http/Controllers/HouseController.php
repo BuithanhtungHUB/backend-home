a1148cd32bb45c5ec655a2a80952461bdabe8510
nguyendacchien <nguyendacchien02032002@gmail.com>
2021-09-01 15:41:06 +0700

    update image

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -4,39 +4,47 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateHouseRequest;
 use App\Models\House;
+use App\Models\Image;
 use App\Models\User;
 
 class HouseController extends Controller
 {
     public function getAll()
     {
-        $houses = House::with('user','category')->get();
+        $houses = House::with('user', 'category')->get();
         return response()->json($houses);
     }
 
     public function getById($id)
     {
-        $house = House::with('user','category')->find($id);
-        return response()->json($house);
+        $house = House::with('user', 'category')->find($id);
+        $images = Image::where('house_id', $id)->get();
+        $data = [$house, $images];
+        return response()->json($data);
     }
 
-    public function create(CreateHouseRequest $request,House $house)
+    public function create(CreateHouseRequest $request, House $house, Image $image)
     {
         $user = User::find($request->user_id);
-        if ($user->role == 'manager'){
+        if ($user->role == 'manager') {
             $house->user_id = $request->user_id;
-            $house->name= $request->name;
+            $house->name = $request->name;
             $house->category_id = $request->category_id;
             $house->address = $request->address;
             $house->bedroom = $request->bedroom;
             $house->bathroom = $request->bathroom;
             $house->description = $request->description;
             $house->price = $request->price;
+            $house->image = $request->image;
             $house->status = $request->status;
             $house->save();
-            return response()->json(['success'=>'Đăng nhà thành công']);
-        }else{
-            return response()->json(['error'=>'bạn không phải là manager']);
+            $image->name = $house->name;
+            $image->house_id = $house->id;
+            $image->url = $request->image;
+            $image->save();
+            return response()->json(['success' => 'Đăng nhà thành công']);
+        } else {
+            return response()->json(['error' => 'bạn không phải là manager']);
         }
     }
 }
