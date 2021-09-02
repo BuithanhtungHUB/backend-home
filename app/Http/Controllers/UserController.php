3b9f663ea3b7fccae94d051fdb857f78c90e681a
nguyendacchien <nguyendacchien02032002@gmail.com>
2021-09-02 16:45:18 +0700

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,18 +11,25 @@ class UserController extends Controller
 {
     public function getHouseList()
     {
-        $id = auth()->user()->id;
-        $user = User::find($id);
-        $houses = House::with('category')->where('user_id', $id)->get();
-        $data = ['user' => $user, 'houses' => $houses];
-        return response()->json($data);
+        if (auth()->user()->role == 'manager') {
+            $id = auth()->user()->id;
+            $user = User::find($id);
+            $houses = House::with('category')->where('user_id', $id)->get();
+            $data = ['user' => $user, 'houses' => $houses];
+            return response()->json($data);
+        } else {
+            return response()->json(['error' => 'ban khong phai manager']);
+        }
     }
 
     public function updateHouse($id, Request $request)
     {
         $house = House::find($id);
-        $house->status = $request->status;
-        $house->save();
-        return response()->json(['success'=>'update thanh cong']);
+        if ($house->status != $request->status) {
+            $house->status = $request->status;
+            $house->save();
+            return response()->json(['success' => 'update thanh cong']);
+        }
+        return response()->json(['error' => 'house dang o trang thai: ' . $request->status]);
     }
 }
